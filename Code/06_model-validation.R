d4faa21dc74051c3e413c34cedf218ea8abdc738
Marc Dotson <marc_dotson@byu.edu>
2021-01-22 17:38:26 -0700

    Added the model-validation R script

---
+++ Code/06_model-validation.R
@@ -0,0 +1,21 @@
+# Load packages.
+library(tidyverse)
+
+# Load data, including validation data.
+data <- read_csv(here::here("Data", "218329_Final_Excel_050619_with_recontact.csv"))
+
+data %>% 
+  select(Acura:Volkswagen) %>% 
+  pivot_longer(Acura:Volkswagen, names_to = "dealership", values_to = "visits") %>% 
+  summarize(
+    max_visits = max(visits),
+    min_visits = min(visits)
+  )
+
+data %>% 
+  select(Acura:Volkswagen) %>% 
+  pivot_longer(Acura:Volkswagen, names_to = "dealership", values_to = "visits") %>% 
+  filter(visits < 20) %>% 
+  ggplot(aes(x = visits)) +
+  geom_bar()
+
