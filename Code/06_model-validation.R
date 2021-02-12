9e7f4d2d89671649fcf6b9abce88d71dc77b10c4
Marc Dotson <marc_dotson@byu.edu>
2021-02-12 15:44:11 -0700

    Setting up for model validation task

--- Code/06_model-validation.R
+++ Code/06_model-validation.R
@@ -4,22 +4,6 @@ library(tidyverse)
 # Load data, including validation data.
 data <- read_csv(here::here("Data", "218329_Final_Excel_050619_with_recontact.csv"))
 
-data %>% 
-  select(Acura:Volkswagen) %>% 
-  pivot_longer(Acura:Volkswagen, names_to = "dealership", values_to = "visits") %>% 
-  summarize(
-    max_visits = max(visits),
-    min_visits = min(visits)
-  )
 
-data %>% 
-  select(Acura:Volkswagen) %>% 
-  pivot_longer(Acura:Volkswagen, names_to = "dealership", values_to = "visits") %>% 
-  filter(visits < 20) %>% 
-  ggplot(aes(x = visits)) +
-  geom_bar()
 
-# Explore Recontact Data
-rec_data <- data %>% 
-  select(contains("REC_Q"))
 
