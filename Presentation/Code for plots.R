e4a6cb479158761de20054b72a096ef09f7699eb
Chris Johnson <cwallacejohnson@gmail.com>
2021-03-26 16:44:14 -0600

    Edited the variable names

--- Presentation/Code for plots.R
+++ Presentation/Code for plots.R
@@ -2,11 +2,11 @@ library(dplyr)
 library(ggplot2)
 
 
-#this uses the modified data frame titled tidy_df as well as the original df found in 02_exploratory-data-analysis.R
+#this uses the modified data frame titled ownership_data as well as the original df titled data found in 02_exploratory-data-analysis.R
 
 #---------------------
 #checking most preferred future car from original data frame
-fav_cars <- tidy_df %>% 
+fav_cars <- ownership_data %>% 
   select(1:8, 412:425, 478:482) 
 
 #-----------------------------------
@@ -39,9 +39,9 @@ kid_chart + coord_polar(theta = "y", start = 0)
 
 #---------------------------------------------------
 
-#likelyhood of purchasing a new vehicle by race
+#likelihood of purchasing a new vehicle by race
 #1 = most likely
-ggplot(tidy_df, aes(x = Purchase_likelihood)) + 
+ggplot(ownership_data, aes(x = Purchase_likelihood)) + 
   geom_bar() +
   xlab("Likelihood\n1: most likely") +
   ggtitle("Likelihood of purchasing a new vehicle\nBy race") +
@@ -49,9 +49,9 @@ ggplot(tidy_df, aes(x = Purchase_likelihood)) +
 
 #------------------------------------------------
 
-#likelyhood of purchasing a used vehicle by race
+#likelihood of purchasing a used vehicle by race
 #1 = most likely
-ggplot(tidy_df, aes(x = Used_vehicle_likelihood)) +
+ggplot(ownership_data, aes(x = Used_vehicle_likelihood)) +
   geom_bar() +
   xlab("Likelihood\n1: most likely") +
   ggtitle("Likelihood of purchasing a used vehicle\nBy race") +
@@ -60,7 +60,7 @@ ggplot(tidy_df, aes(x = Used_vehicle_likelihood)) +
 #--------------------------------------
 
 #anticipated next vehicle price by income
-ggplot(tidy_df, aes(x = Income, y = Anticipated_car_price)) +
+ggplot(ownership_data, aes(x = Income, y = Anticipated_car_price)) +
   geom_bar(stat='identity') + 
   xlim(c(0, 100)) +
   ylim(c(0, 350)) +
@@ -73,12 +73,12 @@ ggplot(tidy_df, aes(x = Income, y = Anticipated_car_price)) +
 #anticipated next vehicle price by age
 
 #making an age variable
-tidy_df <- 
-  tidy_df %>% 
+ownership_data <- 
+  ownership_data %>% 
   mutate(Age = (2020 - Year_born))
 
 #age seems to be a factor
-ggplot(tidy_df, aes(x = Age, y = Anticipated_car_price)) +
+ggplot(ownership_data, aes(x = Age, y = Anticipated_car_price)) +
   geom_histogram(stat='identity') + 
   ylim(c(0, 350)) +
   facet_wrap( ~ Gender)
@@ -86,7 +86,7 @@ ggplot(tidy_df, aes(x = Age, y = Anticipated_car_price)) +
 #----------------------------------------------
 
 #checking most preferred future car from original data frame
-first_car_choice <- df %>% 
+first_car_choice <- data %>% 
   select(Q2x7r1:Q2x7r3)
 
 car_preferences <- first_car_choice %>% 
